7cdd8506f96aba56f4604558b2f5ac62296d537d
Dzmitry Sakalenka <blackvehkleja@gmail.com>
2021-04-21 14:16:26 +0300

    replace 3-1-1.py

--- math and python workshop/6/6-1-1.py
+++ math and python workshop/6/6-1-1.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-'''
+
 V1 = np.fromstring(input(), sep=', ')
 V2 = np.array(V1[-2])
 V3 = V1[::-1]
@@ -29,7 +29,7 @@ A2 = np.array((2, 5))
 A3 = np.array((5, -3))
 
 print(abs(np.cross(A1-A2, A1-A3))/2)
-'''
+
 
 M1 = np.array((
     (1., 2., 3., 0.),
